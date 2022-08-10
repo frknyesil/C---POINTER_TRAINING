790593bbc5c341929219111c13e77406bcb1fdbf
Furkan <99638954+frknyesil@users.noreply.github.com>
2022-08-10 17:07:09 +0300

    Update 2_pointer_variable.c

--- 2_pointer_variable.c
+++ 2_pointer_variable.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int* ptr1;
+    /*Other Correct syntaxes!
+    int * ptr1; 
+    int *ptr1;
+    */
+    
+    int param;
+    ptr1 = &param; //Get address of "param".
+    *ptr1 = 1; //"param" = 1.
+    
+    printf("Variable Value: %d\nPointer Content: %d\n\n", param, *ptr1);
+    printf("Variable Address: %p\nPointed Address: %p", &param, ptr1);
+    
+    return 0;
+}
